0bfcbc743864454e5c87e817d13f458889800136
Thomas Schmitz <thomas.schmitz80@gmail.com>
2018-07-25 00:52:23 +0200

    Add some error handling

--- index.js
+++ index.js
@@ -3,15 +3,37 @@ const https = require('https');
 function doRequest(url) {
     return new Promise((resolve, reject) => {
         https.get(url, (resp) => {
-            let resBody = '';
+            const {statusCode} = resp;
+            const contentType = resp.headers['content-type'];
+
+            let error = null;
+            if (statusCode !== 200) {
+                error = new Error(`Request to ${url} failed with HTTP status: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error(`Content-type of GET ${url} is invalid!`+
+                    ` Expected application/json but received ${contentType}`);
+            }
+
+            if (error) {
+                reject(error);
+                resp.resume();
+                return;
+            }
+
+            let resBody = [];
             resp.on('data', (chunk) => {
-                resBody += chunk;
+                resBody.push(chunk);
             });
             resp.on('end', () => {
-                resolve(JSON.parse(resBody));
+                try {
+                    resolve(JSON.parse(resBody));
+                } catch (err) {
+                    reject(err.message);
+                }
             });
+
         }).on("error", (err) => {
-            reject(err);
+            reject(new Error(`Request to ${url} failed with error: ${err.message}`));
         });
     });
 }
--- test/index.js
+++ test/index.js
@@ -5,13 +5,10 @@ const expect = chai.expect;
 const nock = require('nock');
 
 describe('Get /.well-known/openid-configuration', () => {
-    beforeEach(() => {
+    it('Expect config parsed to object ', () => {
         nock('https://server.example.com')
             .get('/.well-known/openid-configuration')
             .reply(200, require('./openid-configuration.json'));
-    });
-
-    it('Expect config parsed to object ', () => {
         return getOpenIdConfig('https://server.example.com').then(config => {
            expect(typeof config).to.equal('object');
            expect(config.issuer).to.equal('https://server.example.com');
@@ -20,10 +17,37 @@ describe('Get /.well-known/openid-configuration', () => {
     });
 
     it('Expect config parsed to object (also with trailing slash)', () => {
+        nock('https://server.example.com')
+            .get('/.well-known/openid-configuration')
+            .reply(200, require('./openid-configuration.json'));
         return getOpenIdConfig('https://server.example.com/').then(config => {
             expect(typeof config).to.equal('object');
             expect(config.issuer).to.equal('https://server.example.com');
             expect(config.authorization_endpoint).to.equal('https://server.example.com/connect/authorize');
+        });
+    });
+
+    it('Expect error with HTTP status code on wrong issuer URL', () => {
+        nock('https://wrong.example.com')
+            .get('/.well-known/openid-configuration')
+            .reply(404);
+        return getOpenIdConfig('https://wrong.example.com').then(config => {
+            expect(config).to.be.a('null');
+        }).catch(err => {
+            expect(err).to.be.an('error');
+            expect(err.message).to.have.string('404');
+        });
+    });
+
+    it('Expect error with detail message on connection problems with issuer URL', () => {
+        nock('https://wrong.example.c')
+            .get('/.well-known/openid-configuration')
+            .replyWithError('getaddrinfo ENOTFOUND');
+        return getOpenIdConfig('https://wrong.example.c').then(config => {
+            expect(config).to.be.a('null');
+        }).catch(err => {
+            expect(err).to.be.an('error');
+            expect(err.message).to.have.string('getaddrinfo ENOTFOUND');
         });
     });
 });
